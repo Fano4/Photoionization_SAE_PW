204f84f90ee5070bf66827f665ec2fa634c47d02
Stephan van den Wildenberg <svdwildenberg@ulg.ac.be>
2019-09-13 01:34:05 +0200

    Implemented polar derivative. Non checked

--- version_3.0/Computation.cpp
+++ version_3.0/Computation.cpp
@@ -1039,18 +1039,75 @@ std::complex<double> bessel_polarder_contraction_overlap( double k, int jl,int j
    int m3(0);
 
    std::complex<double> sum(0);
+   std::complex<double> sum2(0);
 
    std::complex<double> Kl2( pow(std::complex<double>(0,k),l2) * exp( - k*k / (4 * contraction_zeta) ) / ( pow(2*contraction_zeta,1.5+l2) ) );
 
-   for(int l3=int(fabs(l1-l2));l3!=l1+l2+1;l3++)
+   for(int l3=int(fabs(l1-l2));l3!=l1+l2+2;l3++)
    {
       for(int m3=-l3;m3!=l3+1;l3++)
       {
-         sum+=;
+         if( m3 != 0 )
+         {
+            sum+= 4 * acos(-1) * pow(std::complex<double>(0,-1),l3) * j_l(l3,k*nucl_spher_pos[0])*rYlm(l3,m3,nucl_spher_pos[1],nucl_spher_pos[2])
+               *prefactor_rYlm(l1,m1)*prefactor_rYlm(l2,m2)*prefactor_rYlm(l3,m3)
+               * azim_integ(m1,m2,m3) * ( 
+                     * (3./2.-1./m3) * gaunt_formula(l1,l2,l3,fabs(m1),fabs(m2),fabs(m3)+1) 
+                     + (1./2.-1./m3) * (l3 + m3) * (l3 - m3 + 1) * gaunt_formula(l1,l2,l3-1,fabs(m1),fabs(m2),fabs(m3)) 
+                     );
+         }
+         else
+         {
+            if(l3>0)
+            {
+               sum+=prefactor_rYlm(l1,m1)*prefactor_rYlm(l2,m2)*prefactor_rYlm(l3,m3)
+                  * azim_integ(m1,m2,m3) * ( -gaunt_formula(l1,l2,l3,fabs(m1),fabs(m2),1) );
+            }
+            else
+               sum+=0
+         }
+         if(m2 != 0)
+         {
+            sum2+= 4 * acos(-1) * pow(std::complex<double>(0,-1),l3) * j_l(l3,k*nucl_spher_pos[0])*rYlm(l3,m3,nucl_spher_pos[1],nucl_spher_pos[2])
+               *prefactor_rYlm(l1,m1)*prefactor_rYlm(l2,m2)*prefactor_rYlm(l3,m3)
+               * azim_integ(m1,m2,m3) * ( 
+                     * (3./2.-1./m2) * gaunt_formula(l1,l2,l3,fabs(m1),fabs(m2)+1,fabs(m3)) 
+                     + (1./2.-1./m2) * (l2 + m2) * (l2 - m2 + 1) * gaunt_formula(l1,l2-1,l3,fabs(m1),fabs(m2),fabs(m3)) 
+                     );
+         }
+         else
+         {
+            if(l2 > 0)
+            {
+               sum2+=prefactor_rYlm(l1,m1)*prefactor_rYlm(l2,m2)*prefactor_rYlm(l3,m3)
+                  * azim_integ(m1,m2,m3) * ( -gaunt_formula(l1,l2,l3,fabs(m1),1,fabs(m3)) );
+            }
+            else
+            {
+               sum+=0;
+            }
+         }
       }
    }
 
-   return 
+   return pow(std::complex<double>(0,1),l1)*k*(sum+sum2)*Kl2/4;
+}
+std::complex<double> bessel_azimder_contraction_overlap( double k, int jl,int jml,double* nucl_spher_pos,double contraction_zeta,int* angular_mom_numbers)
+{
+   using namespace std;
+
+   int l1(jl);
+   int l2(angular_mom_numbers[0]);
+   int m1(jml);
+   int m2(angular_mom_numbers[1]);
+   int l3(0);
+   int m3(0);
+
+   std::complex<double> sum(0);
+   std::complex<double> sum2(0);
+
+   std::complex<double> Kl2( pow(std::complex<double>(0,k),l2) * exp( - k*k / (4 * contraction_zeta) ) / ( pow(2*contraction_zeta,1.5+l2) ) );
+
 }
 double azim_integ(int m1,int m2,int m3)
 {
@@ -1091,6 +1148,84 @@ double prefactor_rYlm(int l,int m)
 }
 double gaunt_formula(int l1,int l2,int l3,int m1,int m2,int m3)
 {
+   //DETERMINE THE LARGEST ORDER
+   int temp(0);
+   int u(0);
+   int v(0);
+   int w(0);
+   int l(0);
+   int m(0);
+   int n(0);
+   if(m2>m1)
+   {
+      if(m3>m2) // m3 > m2 > m1
+      {
+         u=m3;v=m2;w=m1;
+         l=l3;m=l2;n=l1;
+      }
+      else if (m3 >m1) // m2 > m3 > m1 
+      {
+         u=m2;v=m3;w=m1;
+         l=l2;m=l3;n=l1;
+      }
+   }
+   else if(m3 > m1) //m3 > m1 > m2
+   {
+         u=m3;v=m1;w=m2;
+         l=l3;m=l1;n=l2;
+   }
+   else
+   {
+      if(m3 > m2) // m1 > m3 > m2
+      {
+         u=m1;v=m3;w=m2;
+         l=l1;m=l3;n=l2;
+      }
+      else // m1 > m2 > m3
+      {
+         u=m1;v=m2;w=m3;
+         l=l1;m=l2;n=l3;
+      }
+   }
+   //CHECK THE ORDER OF THE DEGREES
+   if(m<n)
+   {
+      temp=m;
+      m=n;
+      n=temp;
+      temp=v;
+      v=w;
+      w=temp;
+   }
+
+   //CHECK THAT THE PARAMETERS RESPECT THE CONDITIONS
+   if( m1 < 0 || m2 < 0 || m3 < 0)
+   {
+      std::cout<<"WARNING !! NEGATIVE ORDERS IN GAUNT FORMULA !! "<<std::endl;
+      return 0;
+   }
+   else if (l1 < 0 || l2 < 0 || l3 < 0)
+      rrturn 0;
+   else if( (l+m+n) % 2 != 0)
+      return 0;
+   else if( l > m + n || l < m - n )
+      return 0;
+   else if (u != v + w)
+      return 0;
+
+   //IF THE INTEGRAL IS NOT DEFINED ZERO, COMPUTE THE ACTUAL VALUE
+   int s((l+m+n)/2);
+   int p(std::max(0,n-m-u));
+   int q(std::min(m+n-u,std::min(l-u,n-w)));
+
+   double sum(0);
+
+   for(int t=p;t!=q;t++)
+   {
+      sum+=pow(-1,t)*factorial(l+u+t)*factorial(m+n-u-t)/(factorial(t)*factorial(l-u-t)*factorial(m-n+u+t)*factorial(n-w-t));
+   }
+   return 2*pow(-1,s-m-w)*sum*factorial(m+v)*factorial(n+w)*factorial(2*s-2*n)*factorial(s)/( factorial(m-v)*factorial(s-l)*factorial(s-m)*factorial(s-n)*factorial(2*s+1));
+
 }
 double Dint(int l1,int l2,int l3,int m1,int m2,int m3)
 {
