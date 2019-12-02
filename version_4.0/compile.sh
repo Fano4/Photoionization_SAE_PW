41f3a9852c1f7532ebec49ebd6ea11a7e51ce2ec
Stephan van den Wildenberg <svdwildenberg@ulg.ac.be>
2019-12-02 02:06:54 +0100

    Added compile file

--- version_4.0/compile.sh
+++ version_4.0/compile.sh
@@ -3,7 +3,6 @@
 DEFAULT=/data1/home/stephan/Photoionization_SAE_PW/version_4.0
 SOURCE=/data1/home/stephan/Photoionization_SAE_PW/version_4.0
 
-GSL_ROOT=data1/home/stephan/gsl
 HF5_ROOT=data1/home/stephan/libraries/cpp/hdf5
 
 if [[ -z $1 ]]
@@ -16,5 +15,5 @@ fi
 
 echo "$DIRECTORY"
 icpp=/opt/intel/composer_xe_2015.3.187/bin/intel64/icpc
-${icpp} -g -openmp -I/${GSL_ROOT}/include -I/${MKLROOT}include -I/${MKLROOT}include/fftw -I/${HF5_ROOT}/include -L/${HF5_ROOT}/lib /${HF5_ROOT}/lib/libhdf5_hl_cpp.a /${HF5_ROOT}/lib/libhdf5_cpp.a /${HF5_ROOT}/lib/libhdf5_hl.a /${HF5_ROOT}/lib/libhdf5.a -L/${GSL_ROOT}/lib -L/${MKLROOT}lib/intel64 -Wl,-rpath -Wl,/${HF5_ROOT}/lib -mkl -lgsl -liomp5 -lhdf5 -lhdf5_cpp -lpthread -ldl -lrt -lz -DMKL_Complex16="std::complex<double>"  ${DIRECTORY}/photoion_comp.cpp -o ${DIRECTORY}/PWAPIC_an_orth.exe
+${icpp} -g -openmp -I/${MKLROOT}include -I/${MKLROOT}include/fftw -I/${HF5_ROOT}/include -L/${HF5_ROOT}/lib /${HF5_ROOT}/lib/libhdf5_hl_cpp.a /${HF5_ROOT}/lib/libhdf5_cpp.a /${HF5_ROOT}/lib/libhdf5_hl.a /${HF5_ROOT}/lib/libhdf5.a -L/${MKLROOT}lib/intel64 -Wl,-rpath -Wl,/${HF5_ROOT}/lib -mkl -liomp5 -lhdf5 -lhdf5_cpp -lpthread -ldl -lrt -lz -DMKL_Complex16="std::complex<double>"  ${DIRECTORY}/photoion_comp.cpp -o ${DIRECTORY}/PWAPIC_an_orth.exe
 #${icpp} -g -openmp  -I/${GSL_ROOT}/include -I/${MKLROOT}include -I/${MKLROOT}include/fftw ${HF5_LINKERS} -L/${GSL_ROOT}/lib -L/${MKLROOT}lib/intel64 -mkl -lgsl -liomp5 -lpthread -ldl -DMKL_Complex16="std::complex<double>"  ${DIRECTORY}/photoion_comp.cpp -o ${DIRECTORY}/photoionization_comp.exe
